93ddd09eaea833192b2c66f26b55c4ef1348a786
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-08 20:39:46 +0300

    decomposed

---
+++ build/precache-manifest.d79047b122e34b4d7aea102abf28793b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3222c982da3a39d79fda2a9f50ced4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "04d97c2754e2aa079e91",
+    "url": "./static/css/main.e6cce7a3.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "04d97c2754e2aa079e91",
+    "url": "./static/js/main.e82fbdc3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
