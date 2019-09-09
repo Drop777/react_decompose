b449700405081ffad14b7157e4a8577d61af0cb9
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-09 13:49:57 +0300

    Fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './Navigation.js';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
 
 function Header () {
   return (
     <header className="header">
         <h1 className="header__title">Site Name</h1>
-        <Navigation/>
+        <Navigation />
       </header >
   )
 }
